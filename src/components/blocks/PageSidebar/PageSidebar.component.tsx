a06ebc2769d34fbca22fb5ac2a966275bd06ec14
Jindřich Kraft <jindrich.kraft@proton.me>
2023-04-04 08:45:47 +0200

    Setting up the sidebar layout

--- src/components/blocks/PageSidebar/PageSidebar.component.tsx
+++ src/components/blocks/PageSidebar/PageSidebar.component.tsx
@@ -3,6 +3,34 @@ import './PageSidebar.styles.scss';
 const PageSidebar = (): JSX.Element => (
   <nav className="page-sidebar">
     <h2>Sidebar</h2>
+    <p className="board-count-label">All Boards (5)</p>
+    <ul className="sidebar-items">
+      <li className="nav-item">
+        <a href="#" className="nav-link">
+          Board Name
+        </a>
+      </li>
+      <li className="nav-item">
+        <a href="#" className="nav-link">
+          Board Name
+        </a>
+      </li>
+      <li className="nav-item">
+        <a href="#" className="nav-link">
+          Board Name
+        </a>
+      </li>
+      <li className="nav-item">
+        <a href="#" className="nav-link">
+          Board Name
+        </a>
+      </li>
+      <li className="nav-item">
+        <a href="#" className="nav-link">
+          Board Name
+        </a>
+      </li>
+    </ul>
   </nav>
 );
 
