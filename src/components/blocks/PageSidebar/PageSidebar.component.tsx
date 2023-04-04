252bc5a97566436444739b0d1b55011736633e28
Jindřich Kraft <jindrich.kraft@proton.me>
2023-04-04 10:16:03 +0200

    Styling the sidebar

--- src/components/blocks/PageSidebar/PageSidebar.component.tsx
+++ src/components/blocks/PageSidebar/PageSidebar.component.tsx
@@ -1,36 +1,57 @@
+import Icon from '../../elements/Icon';
+
 import './PageSidebar.styles.scss';
 
 const PageSidebar = (): JSX.Element => (
   <nav className="page-sidebar">
-    <h2>Sidebar</h2>
+    <div className="sidebar-logo-placeholder" />
     <p className="board-count-label">All Boards (5)</p>
     <ul className="sidebar-items">
-      <li className="nav-item">
-        <a href="#" className="nav-link">
+      <li className="sidebar-item active">
+        <div className="board-icon">
+          <Icon name="chess-board" type="fas" />
+        </div>
+        <a href="#" className="sidebar-link">
           Board Name
         </a>
       </li>
-      <li className="nav-item">
-        <a href="#" className="nav-link">
+      <li className="sidebar-item">
+        <div className="board-icon">
+          <Icon name="chess-board" type="fas" />
+        </div>
+        <a href="#" className="sidebar-link">
           Board Name
         </a>
       </li>
-      <li className="nav-item">
-        <a href="#" className="nav-link">
+      <li className="sidebar-item">
+        <div className="board-icon">
+          <Icon name="chess-board" type="fas" />
+        </div>
+        <a href="#" className="sidebar-link">
           Board Name
         </a>
       </li>
-      <li className="nav-item">
-        <a href="#" className="nav-link">
+      <li className="sidebar-item">
+        <div className="board-icon">
+          <Icon name="chess-board" type="fas" />
+        </div>
+        <a href="#" className="sidebar-link">
           Board Name
         </a>
       </li>
-      <li className="nav-item">
-        <a href="#" className="nav-link">
+      <li className="sidebar-item">
+        <div className="board-icon">
+          <Icon name="chess-board" type="fas" />
+        </div>
+        <a href="#" className="sidebar-link">
           Board Name
         </a>
       </li>
     </ul>
+    <div className="theme-switch-box">
+      <Icon name="sun" type="fas" />
+      <Icon name="moon" type="fas" />
+    </div>
   </nav>
 );
 
